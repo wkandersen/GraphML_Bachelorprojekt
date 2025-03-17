58937446a25a028585d8b67eb1af89132ef0b6ae
Kirk <s224225@dtu.dk>
2025-03-17 11:01:34 +0100

    loading af data og filtering

--- src/data.py
+++ src/data.py
@@ -1,11 +1,11 @@
 from ogb.nodeproppred import PygNodePropPredDataset
 
 # Load the dataset
-dataset = PygNodePropPredDataset(name = "ogbn-mag")
+dataset = PygNodePropPredDataset(name = "ogbn-mag", root = "dataset/")
 
-split_idx = dataset.get_idx_split()
-train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
-graph = dataset[0] # pyg graph object
+# split_idx = dataset.get_idx_split()
+# train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
+# graph = dataset[0] # pyg graph object
 
 
-print(graph.num_nodes_dict)
+# print(graph.num_nodes_dict)
