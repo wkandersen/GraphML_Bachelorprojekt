3aa8ff20a3e05f2c4336af185bcae78a612d23a9
Kirk <s224225@dtu.dk>
2025-03-31 14:54:22 +0200

    bash script og enviroment

---
+++ HPC_Scripts/script1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -J Graph_ML_Bachelor
+#BSUB -o ML_bachelor_%J.out
+#BSUB -e ML_bachelor_%J.err
+#BSUB -q hpc
+#BSUB -R "rusage[mem=8G]"
+#BSUB -R "span[hosts=4]"
+#BSUB -W 12:00
+#BSUB -n 4
+#BSUB -B
+#BSUB -N
+#BSUB -u williamkirkandersen@gmail.com
+
+
+
+
+
