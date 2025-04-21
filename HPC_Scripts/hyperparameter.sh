c312247f14116fd6337ddc685a3633bd425747b3
Kristine <s224197@dtu.dk>
2025-04-21 12:21:42 +0200

    hyperparameter sweep

--- HPC_Scripts/hyperparameter.sh
+++ HPC_Scripts/hyperparameter.sh
@@ -7,15 +7,16 @@
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -n 4
-#BSUB -W 12:00
+#BSUB -W 24:00
 #BSUB -B
 #BSUB -N
-#BSUB -u williamkirkandersen@gmail.com
+#BSUB -u s224197@dtu.dk
 
 export PYTHONPATH=$PYTHONPATH:$(pwd)/src:$(pwd)/Packages:$(pwd)/dataset
 
 source ~/miniconda3/bin/activate
-
 conda activate Bachelorprojekt
+
+export WANDB_API_KEY="b26660ac7ccf436b5e62d823051917f4512f987a"
 
 python src/model1/wandb_hyper_sweep.py
