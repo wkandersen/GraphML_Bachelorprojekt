45bf07fc3e68176ae8b5cfe109694d33f782931e
William <s224225@dtu.dk>
2025-05-12 09:24:59 +0200

    embed valid fix

--- src/model1/embed_valid.py
+++ src/model1/embed_valid.py
@@ -8,14 +8,14 @@ from Packages.data_divide import paper_c_paper_train, paper_c_paper_valid, data
 import wandb
 import torch.nn as nn
 
-paper_dict = torch.load('/mnt/c/Users/Bruger/Desktop/Bachelor/GraphML_Bachelorprojekt/dataset/ogbn_mag/processed/hpc/collected_embeddings_2.pt')
+paper_dict = torch.load('checkpoint/checkpoint_iter_2_25_epoch_1.pt')
 
 learning_rate = 0.001
 num_epochs = 100
 alpha = 0.1
 eps = 0.001
 lam = 0.01
-batch_size = 1
+batch_size = 5
 num_iterations = 10
 emb_dim = 2
 
@@ -56,37 +56,40 @@ for i in range(num_iterations):
     mean_tensor = None
     count = 0
 
-if len(dm[:, 2].tolist()) < 2:
-    new_embedding = nn.Embedding(1,emb_dim)
-else:
-    # Iterate over the elements in dm[:, 2]
-    for i in dm[:, 2].tolist():
-        tensor_value = paper_dict['paper'][i]  # Access the tensor from paper_dict
-        if mean_tensor is None:
-            mean_tensor = tensor_value.clone()  # Initialize the mean_tensor with the first tensor
-        else:
-            mean_tensor += tensor_value  # Accumulate the sum of tensors
-        count += 1  # Increment the count of tensors
+# if len(dm[:, 2].tolist()) < 2:
+#     new_embedding = nn.Embedding(1,emb_dim)
+# else:
+#     # Iterate over the elements in dm[:, 2]
+#     for i in dm[:, 2].tolist():
+#         tensor_value = paper_dict['collected_embeddings']['paper'][i]  # Access the tensor from paper_dict
+#         if mean_tensor is None:
+#             mean_tensor = tensor_value.clone()  # Initialize the mean_tensor with the first tensor
+#         else:
+#             mean_tensor += tensor_value  # Accumulate the sum of tensors
+#         count += 1  # Increment the count of tensors
 
-    # Compute the element-wise mean by dividing the accumulated sum by count
-    mean_tensor /= count
+#     # Compute the element-wise mean by dividing the accumulated sum by count
+#     mean_tensor /= count
     
-    new_embedding = nn.Parameter(mean_tensor)
+#     new_embedding = nn.Parameter(mean_tensor)
+
+    new_embedding = torch.nn.Embedding(1,2).weight
 
     # Now mean_tensor holds the element-wise mean of all tensors
     print(f"Element-wise mean tensor: {mean_tensor}")
-
-    paper_dict['paper'][random_sample[0]] = torch.nn.Parameter(new_embedding.clone().detach())
-    print(paper_dict['paper'][random_sample[0]])
-    new_optimizer = torch.optim.Adam([paper_dict['paper'][random_sample[0]]], lr=learning_rate)
+    
+    paper_dict['collected_embeddings']['paper'][random_sample[0]] = new_embedding
+    print(paper_dict['collected_embeddings']['paper'][random_sample[0]])
+    new_optimizer = torch.optim.Adam([paper_dict['collected_embeddings']['paper'][random_sample[0]]], lr=learning_rate)
 
     # Training loop for multiple samples
     for epoch in range(num_epochs):
         new_optimizer.zero_grad()
+        
 
         # temp_embed = torch.stack(list(paper_dict.values())) # SKAL ÆNDRES
         # Concatenate the embeddings
-        loss = loss_function.compute_loss(paper_dict, final)  # Compute loss
+        loss = loss_function.compute_loss(paper_dict['collected_embeddings'], final)  # Compute loss
 
         # Backpropagation and optimization
         loss.backward()
@@ -97,7 +100,7 @@ else:
     if epoch % 10 == 0:
         print(f"Epoch {epoch}: Loss = {loss.item():.4f}")
 
-    print(paper_dict['paper'][random_sample[0]])
+    print(paper_dict['collected_embeddings']['paper'][random_sample[0]])
 
     l_prev = l_next
 
